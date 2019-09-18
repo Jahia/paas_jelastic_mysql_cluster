febcbd9d54b723a90a32c096f04b84da31fe0b5c
Ronan GORAIN <rgorain@jahia.com>
2019-09-18 15:35:47 +0200

    PAAS-318: Update MySQL SetupUser script to skip (long) root user setup

--- scripts/setupUser.sh
+++ scripts/setupUser.sh
@@ -2,15 +2,7 @@
 
 USER=$1
 PASSWORD=$2
-ADMIN_PASSWORD=`pwgen 10 1`
-JEM=`which jem`
+
 MYSQL=`which mysql`
 cmd="CREATE USER '$USER'@'localhost' IDENTIFIED BY '$PASSWORD'; CREATE USER '$USER'@'%' IDENTIFIED BY '$PASSWORD'; GRANT ALL PRIVILEGES ON *.* TO '$USER'@'localhost' WITH GRANT OPTION; GRANT ALL PRIVILEGES ON *.* TO '$USER'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
-unset resp;
-resp=$(mysql -u$USER -p$PASSWORD mysql --execute="SHOW COLUMNS FROM user")
-[ -z "$resp" ] && {
-   $JEM passwd set -p $ADMIN_PASSWORD
-   $MYSQL -uroot -p${ADMIN_PASSWORD} --execute="$cmd"
-} || {
-   echo "[Info] User $user has the required access to the database."
-}
+$MYSQL --execute="$cmd"
