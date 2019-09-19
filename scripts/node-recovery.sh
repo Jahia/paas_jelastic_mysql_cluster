282331bd0d0d7a8fb5f6f3808ff63f9899a0ac76
Ronan GORAIN <rgorain@jahia.com>
2019-09-19 18:10:13 +0200

    PAAS-318: add more checks to the restore node script

--- scripts/node-recovery.sh
+++ scripts/node-recovery.sh
@@ -2,8 +2,18 @@
 
 export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin
 
-#TODO: add checks
-/usr/local/orchestrator/orchestrator -c set-read-only -i $1
-/usr/local/orchestrator/orchestrator -c end-downtime -i $1
-/usr/local/orchestrator/orchestrator -c ack-instance-recoveries -i $1 --reason "node is back"
+if [ "$1" != "-n" ] || [ $# -ne 2 ];then
+    echo "usage: node-recovery -n nodeXXXX"
+    exit 1
+fi
+
+NODE_ID="$2"
+
+# Get how many nodes are currently downtimed and are not in read only or the node we are trying to recover
+downtimed=$(/usr/local/orchestrator/orchestrator -c topology -i $NODE_ID|grep -v -e downtimed -e ",ro," -e "$NODE_ID"|wc -l)
+if [[ $downtimed -ne 0 ]]; then # set in read only if there is already online nodes (which are not in downtime)
+    /usr/local/orchestrator/orchestrator -c set-read-only -i $NODE_ID
+fi
+/usr/local/orchestrator/orchestrator -c end-downtime -i $NODE_ID
+/usr/local/orchestrator/orchestrator -c ack-instance-recoveries -i $NODE_ID --reason "node is back"
 
